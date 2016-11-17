56ffae48f63ba74d7ad00b088c006360d63628e6
TaimoorSaeed <taimoor_saeed@hotmail.com>
2016-11-17 23:09:21 +0500

    initial commit

---
+++ Generics Example Swift.playground/Contents.swift
@@ -0,0 +1,32 @@
+// Generics code to write flexible , resuable functions and structures
+
+//Example
+
+var stringArray = ["Hi", "Hello" , "Bye"]
+var intArray = [1,2,3,4,5,6]
+var doubleArray = [1.2,3.1,2.1]
+
+// Print All the elements
+
+func printStringFromArray (a:[String]){
+    for s in a{
+        print(s)
+    }
+}
+
+func printIntFromArray (a:[Int])
+{
+    for i in a {
+        print(i)
+    }
+}
+
+func printDoubleFromArray(a:[Double]){
+    for d in a {
+        print(d)
+    }
+}
+
+
+
+
